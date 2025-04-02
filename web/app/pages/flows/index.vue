57e9b03c99182cc1ff31809508747f3a44039539
Istvan Szana <szana8@gmail.com>
2025-04-02 10:43:33 +0200

    -

--- web/app/pages/flows/index.vue
+++ web/app/pages/flows/index.vue
@@ -1,5 +1,50 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div>Flows</div>
+    <div class="p-4 space-y-10">
+        <div class="flex justify-between">
+            <h1 class="text-3xl font-light">Flows</h1>
+            <button class="border border-gray-900 rounded-lg p-2 text-sm text-gray-900 hover:bg-gray-200">Add
+                flow</button>
+        </div>
+
+        <div>
+            <table class="w-full text-left">
+                <thead class="border-b border-gray-200">
+                    <th class="py-2">Name</th>
+                    <th class="py-2">Description</th>
+                    <th class="py-2">Link</th>
+                    <th class="py-2">Langflow ID</th>
+                    <th class="py-2">Enabled</th>
+                    <th class="py-2">Action</th>
+                </thead>
+                <tbody>
+                    <tr class="text-gray-600 items-center">
+                        <td class="py-4">Text to image</td>
+                        <td class="py-4">Lorem ipsum</td>
+                        <td class="py-4">http://localhost:8090/api/run/flow/0195f0af-d929-7169-a43a-60ae7b06d7fc</td>
+                        <td class="py-4">0195f0af-d929-7169-a43a-60ae7b06d7fc</td>
+                        <td class="py-4">True</td>
+                        <td class="p-4 flex items-center space-x-4">
+                            <button class="text-gray-900 hover:text-gray-400">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                </svg>
+                            </button>
+                            <button class="text-red-900 hover:text-red-400">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                </svg>
+
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
